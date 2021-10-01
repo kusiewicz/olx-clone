6ec030851bf1c69596106f7dc7eb8d184f8fb149
Kusy <kamil.kusy111@gmail.com>
2021-10-01 13:56:48 +0200

    NavBar visibility handling moved to the NavBar component from 'Pages' to avoid unnecessary re-rendering of components on Home Page

--- src/Containers/NavBar/index.js
+++ src/Containers/NavBar/index.js
@@ -1,38 +1,54 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import { Container, Logo, Icon, Button, SubLogo } from '../../Components';
 
-const NavBar = ({ hidden }) => (
-  <>
-    {/* Padding needed due to NavBar fixed position */}
-    <Container className={styles.padded} />
+const NavBar = () => {
+  const [scrolled, setScrolled] = useState(false);
 
-    <Container id="header" className={`${styles.nav} ${hidden && styles.hidden}`}>
-      <Container className={`flex ${styles.wrapper}`}>
-        <a href="http://www.olx.pl" className="rel">
-          <Logo color="turquoise" size="small" />
-          <SubLogo />
-        </a>
-        <Container className={`flex cntr ${styles.userPanel}`}>
-          <Container className={styles.hover}>
-            <Icon className={`icon-comment rel ${styles.messageIcon}`} />
-            <span className={`rel ${styles.message}`}>Wiadomości</span>
+  /* Disappearing NavBar */
+  const navBarVisibilityHandler = () => {
+    const scrollVertical = window.scrollY;
+    if (scrollVertical > 0) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', navBarVisibilityHandler);
+
+    return () => {
+      window.removeEventListener('scroll', navBarVisibilityHandler);
+    };
+  });
+  return (
+    <>
+      {/* Padding needed due to NavBar fixed position */}
+      <Container className={styles.padded} />
+
+      <Container id="header" className={`${styles.nav} ${scrolled && styles.hidden}`}>
+        <Container className={`flex ${styles.wrapper}`}>
+          <a href="http://www.olx.pl" className="rel">
+            <Logo color="turquoise" size="small" />
+            <SubLogo />
+          </a>
+          <Container className={`flex cntr ${styles.userPanel}`}>
+            <Container className={styles.hover}>
+              <Icon className={`icon-comment rel ${styles.messageIcon}`} />
+              <span className={`rel ${styles.message}`}>Wiadomości</span>
+            </Container>
+            <Icon className={`icon-heart-empty white rel ${styles.heartIcon} ${styles.hover}`} />
+            <Container className={styles.hover}>
+              <Icon className={`icon-user-o white ${styles.userIcon}`} />
+              <span className={`rel ${styles.olxText}`}>Mój OLX</span>
+            </Container>
+            <Button className={styles.btn}>Dodaj ogłoszenie</Button>
           </Container>
-          <Icon className={`icon-heart-empty white rel ${styles.heartIcon} ${styles.hover}`} />
-          <Container className={styles.hover}>
-            <Icon className={`icon-user-o white ${styles.userIcon}`} />
-            <span className={`rel ${styles.olxText}`}>Mój OLX</span>
-          </Container>
-          <Button className={styles.btn}>Dodaj ogłoszenie</Button>
         </Container>
       </Container>
-    </Container>
-  </>
-);
-
-NavBar.propTypes = {
-  hidden: PropTypes.bool,
+    </>
+  );
 };
 
 export default NavBar;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,38 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import { NavBar, Search, Categories, PromotedAds, Jumbotron, Footer } from '../Containers';
 
-const Home = () => {
-  const [scrolled, setScrolled] = useState(false);
-
-  /* Disappearing NavBar */
-  const navBarVisibilityHandler = () => {
-    const scrollVertical = window.scrollY;
-    if (scrollVertical > 0) {
-      setScrolled(true);
-    } else {
-      setScrolled(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', navBarVisibilityHandler);
-
-    return () => {
-      window.removeEventListener('scroll', navBarVisibilityHandler);
-    };
-  });
-
-  return (
-    <>
-      <NavBar hidden={scrolled} />
-      <Search />
-      <Categories />
-      <PromotedAds />
-      <Jumbotron />
-      <Footer />
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <NavBar />
+    <Search />
+    <Categories />
+    <PromotedAds />
+    <Jumbotron />
+    <Footer />
+  </>
+);
 
 export default Home;
